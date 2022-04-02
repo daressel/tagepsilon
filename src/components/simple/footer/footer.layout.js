5c3b1e6169e78073f732235fa35fe337cc4027b3
daressel <nevaliashkaoxxxy@gmail.com>
2022-04-02 22:16:41 +0300

    Simple footer

--- src/components/simple/footer/footer.layout.js
+++ src/components/simple/footer/footer.layout.js
@@ -1,10 +1,29 @@
 import { memo } from 'react';
-import { FooterWrapper } from './footer.styled';
+import {
+  FooterWrapper,
+  SocialWrapper,
+  SocialItem,
+  Title,
+} from './footer.styled';
+import SVGIcon from '../../ui/svg/svg';
 
 const FooterLayout = () => {
   return (
     <>
-      <FooterWrapper></FooterWrapper>
+      <FooterWrapper>
+        <Title>Epsilon</Title>
+        <SocialWrapper>
+          <SocialItem>
+            <SVGIcon src="icons/vk_icon.svg" />
+          </SocialItem>
+          <SocialItem>
+            <SVGIcon src="icons/telegram_icon.svg" />
+          </SocialItem>
+          <SocialItem>
+            <SVGIcon src="icons/whatsapp_icon.svg" />
+          </SocialItem>
+        </SocialWrapper>
+      </FooterWrapper>
     </>
   );
 };
--- src/components/simple/footer/footer.styled.js
+++ src/components/simple/footer/footer.styled.js
@@ -5,7 +5,22 @@ import stylesProps from '../../../core/constants/stylesProps';
 export const FooterWrapper = styled.div`
   width: 100%;
   display: flex;
+  justify-content: space-between;
   bottom: 0px;
   background-color: ${colors.Primary};
   min-height: ${stylesProps.footerHeight};
+`;
+
+export const Title = styled.div`
+  align-self: center;
+  padding: 15px;
+  color: ${colors.White};
+`;
+
+export const SocialWrapper = styled.div`
+  display: flex;
+`;
+
+export const SocialItem = styled.div`
+  display: flex;
 `;
