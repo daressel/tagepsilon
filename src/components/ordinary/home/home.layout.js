9b6944953d53d49d11b1a3e4b85acb86edab0cc0
daressel <nevaliashkaoxxxy@gmail.com>
2022-05-09 21:42:05 +0300

    Remove unused code

--- src/components/ordinary/home/home.layout.js
+++ src/components/ordinary/home/home.layout.js
@@ -1,28 +1,21 @@
 import { memo } from 'react';
 import PageWrapper from '../../smart/pageWrapper/pageWrapper';
-import Services from '../../simple/services/services';
 import { HomeWrapper, SubTitle, Title, LogoWrapper } from './home.styled';
 import CategoryProducts from '../../simple/categoryProducts/categoryProducts';
 import AboutUs from '../about-us/about-us';
-import Product from '../../simple/product/product';
 
-const HomeLayout = ({ selectedItem, handleSelectItem }) => {
+const HomeLayout = () => {
   return (
     <>
       <PageWrapper>
         <HomeWrapper>
-          {!selectedItem && (
-            <>
-              <LogoWrapper />
-              <AboutUs />
-              <Title>ООО ЭПСИЛОН </Title>
-              <SubTitle>предлагает выполнение работ</SubTitle>
-              <CategoryProducts handleSelectItem={handleSelectItem} />
-            </>
-          )}
-          {selectedItem && <Product product={selectedItem} handleSelectItem={handleSelectItem} />}
-
-          {/* <Services selectedItem={selectedItem} handleSelectItem={handleSelectItem} /> */}
+          <>
+            <LogoWrapper />
+            <AboutUs />
+            <Title>ООО ЭПСИЛОН </Title>
+            <SubTitle>предлагает выполнение работ</SubTitle>
+            <CategoryProducts />
+          </>
         </HomeWrapper>
       </PageWrapper>
     </>
